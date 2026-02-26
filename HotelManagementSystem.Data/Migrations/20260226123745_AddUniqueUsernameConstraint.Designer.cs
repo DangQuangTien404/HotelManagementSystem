b8d1efa86574174eacad8782d0e18038445b4a54
DangQuangTien <dangquangtien16102002@gmail.com>
2026-02-26 19:46:27 +0700

    fix

--- HotelManagementSystem.Data/Migrations/20260226123745_AddUniqueUsernameConstraint.Designer.cs
+++ HotelManagementSystem.Data/Migrations/20260226123745_AddUniqueUsernameConstraint.Designer.cs
@@ -4,6 +4,7 @@ using HotelManagementSystem.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelManagementSystem.Data.Migrations
 {
     [DbContext(typeof(HotelManagementDbContext))]
-    partial class HotelManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260226123745_AddUniqueUsernameConstraint")]
+    partial class AddUniqueUsernameConstraint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
